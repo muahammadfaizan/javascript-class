cf68b6029c1dcd31442e9c2d38f2c400ab2e18be
Muhammad Faizan <fiazansuri855@gmail.com>
2024-03-09 00:52:46 +0500

    JS-class-02

---
+++ class-02/app.js
@@ -0,0 +1,183 @@
+// var num1 = +prompt('Enter first Number');
+// var num2 = +prompt('Enter second Number');
+// var num3 = +prompt('Enter third Number');
+// var num4 = +prompt('Enter fourth Number');
+
+
+
+// console.log(num1 + num2 + num3 + num4);
+
+// alert('hello world')
+
+// var num1  = +prompt('first number');
+// var num2  = +prompt('multiply by');
+// var num3  = +prompt('is =') 
+
+// console.log(num1 > num2);
+
+
+// var num1= 15;
+// var num2 = 20;
+// var num3 = 10;
+// var num4 = 5;
+// var num5 = 2;
+
+
+
+
+// console.log(10 + 20 - 10 * 5 / 2);
+
+
+
+// console.log('hello world');
+
+// var userName = 'abdullah';
+// console.log(userName)
+
+
+// alert('hello world');
+// alert('hello world');
+
+
+// var userName = ('hello world');
+// console.log('userName')
+
+// var num1 = 10;
+// var num2 = '20';
+// var num3 = 20;
+
+// console.log(typeof(num1))
+// console.log(num1 + num2 + num3)
+
+
+
+
+
+// var username = prompt('Enter Your Name!');
+// console.log(username);
+
+
+
+
+
+// var num1 = Number(prompt('Enter first number'));
+// var num2 = Number(prompt('Enter second number'));
+
+
+// var num1 = +prompt('Enter first number');
+// var num2 = +prompt('Enter second number');
+// console.log(num1 + num2);
+
+
+
+
+
+//Arithmatic Operator
+
+// +
+// -
+// *
+// /
+// % == modulus
+
+// var num1 = 10;
+// var num2 = 5;
+
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+// console.log('modulus ===> ', num1 % num2);
+// console.log(10 + 20 - 10 * 5 / 2);
+
+
+
+
+
+
+
+
+
+
+
+
+//Assignment Operator
+// =
+// +=
+// -=
+// /=
+// *=
+
+// var num1 = 12;
+// num1 = num1 + 10;
+// num1 += 10;
+// num1 -= 10;
+// num1 *= 10;
+// num1 /= 10;
+// console.log(num1);
+
+
+
+//Comparision Operator
+
+// >
+// <
+// == || !=
+// === || !==
+// != data type check nahi kraiga!
+// !== data type check kraiga!
+// >=
+// <=
+
+// console.log(10 == '10');
+// console.log(10 === '10');
+
+// console.log(10 != 10);
+// console.log(10 == '10');
+// var username = 'abdullah'
+// document.write('<h1>'+ username+ '</h1>');
+
+
+
+
+//logical Operator
+
+//&& == and
+//|| == OR
+
+
+// console.log(10 > 5 || 10 < 15 || 15 > 10);
+// console.log(10 > 5 && 10 < 15 && 5 > 10);
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  user five subject marks 
+
+var chem = +prompt('Enter your marks chemistry');
+var bio = +prompt('Enter your marks biology');
+var Maths = +prompt('Enter your marks Maths');
+var Urdu = +prompt('Enter your marks Urdu');
+var Phy = +prompt('Enter your marks Physics');
+  
+var totalsubject = chem + bio + Maths + Urdu + Phy ; 
+
+var totalmarks = 500 ;
+
+console.log(totalsubject/totalmarks*100);
+
+
+
+
+
+
+
