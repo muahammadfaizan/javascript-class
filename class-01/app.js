c10de541e095cd296e920914cf9eef3a69b35600
Muhammad Faizan <fiazansuri855@gmail.com>
2024-03-09 00:50:12 +0500

    JS-class-01

---
+++ class-01/app.js
@@ -0,0 +1,13 @@
+alert("hello world");
+
+
+
+
+var firstName = "Muhammad";
+var lastName = "faizan";
+var age = 20 ;
+
+var  isAvailable = true ;
+
+
+console.log('my name is ' + firstName + " " + lastName + ' my age is ' + age);
