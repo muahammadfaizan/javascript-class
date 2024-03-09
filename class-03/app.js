2401385a91a74de144f5c5af7d43c50ef7aaeb39
Muhammad Faizan <fiazansuri855@gmail.com>
2024-03-09 22:11:02 +0500

    JS class 03

---
+++ class-03/app.js
@@ -0,0 +1,61 @@
+// console.log('hello world');
+
+
+// var percentage = +prompt ("enter your percentage");
+
+// if(percentage > 100) {
+//     console.log(
+//         "bhai percentage sahi dal de"
+//     );
+// }
+// else(percentage < 50){
+//     console.log(
+//         "good"
+//     );
+// }
+
+
+var percentage = +prompt('enter your percentage');
+
+if(percentage > 100) {
+    console.log('bhai percentage to sahi dal de');
+ 
+} else if(percentage >= 50 && percentage <= 60){
+    console.log('C grade aya hai');
+} else if(percentage >= 61 && percentage <= 70){
+    console.log('B grade aya hai bhai');
+} else if (percentage >= 71 && percentage <= 80){
+    console.log('A grade aya hai');
+} else if(percentage >= 81 && percentage <= 90){
+    console.log('A-One grade aya hai');
+} else if(percentage >= 91 && percentage <= 100){
+    console.log('A+ grade aya hai');
+}
+    
+else{(percentage < 50) 
+      console.log('bhai ap fail hogaye!');
+}
+
+// if (percentage >= 60){
+//     console.log('b grade aya hai');
+// }
+
+
+
+
+
+
+
+
+// var num = +prompt('enter your number');
+
+// if(num % 2 === 0){
+//     console.log(
+//         'num even h bhai'
+//     );
+// }
+// else{
+//     console.log(
+//         'num odd h bhai'
+//     );
+// }
