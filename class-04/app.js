b76ba9559a5141e3bf1eb9cd9d65c40ae4bcaf24
Muhammad Faizan <fiazansuri855@gmail.com>
2024-03-10 00:04:57 +0500

    JS class 04

---
+++ class-04/app.js
@@ -0,0 +1,206 @@
+// console.log('hello world');
+
+
+// function sum (num1 , num2){
+//     console.log("sum of two number ===>" , num1 + num2);
+    
+// }
+// sum(19 , 20);
+
+
+// function showstring(){
+//     console.log("hello world");
+
+//     return "Muhammad";
+// }
+// showstring()
+// var myfunc = showstring()
+// console.log(myfunc);
+
+
+
+// function shownumber(){
+//     console.log('hello world 2');
+//     return "bhai javascript easy ha ye hard";  
+// }
+
+// shownumber()
+// var myshow = shownumber();
+// console.log(myshow)
+
+    //  percentage calculate class 
+
+
+// function percentageCal(marks1 , marks2 , marks3 , marks4){       
+//    var totalmarks = 400;
+//    var obtainedmarks = marks1 + marks2 + marks3 + marks4; 
+//    return obtainedmarks / totalmarks  * 100; 
+// }
+// var muhammadPercentage = percentageCal(60 , 65 , 79 , 84);
+
+// console.log(muhammadPercentage);
+
+
+
+
+// function percentageCal(sub1 , sub2 , sub3 , sub4){
+//      var totalmarks = 400;
+//      var obtainedmarks = sub1 + sub2 + sub3 + sub4;
+//      return obtainedmarks / totalmarks * 100;
+// }
+
+// var muhammadPercentage = percentageCal(95 , 89 , 98 , 92);
+// console.log(muhammadPercentage);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var head = document.querySelector('.head');
+// var paragraph = document.querySelector('#para');
+// console.log(paragraph.innerHTML);
+
+
+// function contenttext(){
+//     if(paragraph.innerHTML === 'change through js' && head.innerHTML === 'change through js'){
+//         paragraph.innerHTML = 'lorem ispum pakistan';
+//         head.innerHTML = 'hello world';
+//     }
+//     else{
+//         paragraph.innerHTML = 'change through js';
+//         head.innerHTML = 'change through js';
+//     }
+// }
+
+
+
+
+
+
+var head = document.querySelector('.head');
+var paragraph = document.querySelector('#para');
+console.log(head.innerHTML);
+console.log(paragraph.innerHTML);
+
+
+function contenttext(){
+    if(paragraph.innerHTML === 'change the js' && head.innerHTML === 'change the js'){
+        paragraph.innerHTML = 'lorem ispum pakistan';
+        head.innerHTML = 'hello world';
+    }
+    else{
+        paragraph.innerHTML = "change through js";
+        head.innerHTML = "change through js";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
