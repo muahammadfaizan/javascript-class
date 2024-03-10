7deafec77149d125020440fd4323bd8eb8b0529c
Muhammad Faizan <fiazansuri855@gmail.com>
2024-03-11 01:29:57 +0500

    JS class 05

---
+++ class-05/app.js
@@ -0,0 +1,209 @@
+// var h1 = document.querySelector('.head');
+// // h1.innerHTML = "change through js"
+// // console.log(h1.innerHTML);
+
+
+
+
+
+// var paragraph = document.querySelector('.para');
+// // paragraph.innerHTML = 'lorem ispum';
+// // console.log(paragraph.innerHTML);
+
+// function  contentText(){
+//     if(h1.innerHTML === 'Hello world' && paragraph.innerHTML === 'Lorem ipsum dolor sit.'){
+//         h1.innerHTML = "change through js" 
+//         paragraph.innerHTML = "change through js" 
+//     }
+//     else{
+//         h1.innerHTML = 'Hello world';
+//         paragraph.innerHTML = 'Lorem ipsum dolor sit.';
+//        console.log(h1.innerHTML);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var input = document.querySelector('.input');
+// console.log(input.value) 
+
+
+// function  getInput(){
+//     console.log(input.value);
+//     input.value = ""
+// }
+
+
+
+
+
+
+
+// Template literals
+// backtick
+
+
+// var firstName = "Muhammad";
+// var lastName = "Khan";
+// var age = 19
+
+// console.log(`my name is ${firstName} ${lastName} and my age is ${age}`);
+
+
+
+
+
+
+
+
+// var chem = +prompt('Enter your marks chemistry');
+// var bio = +prompt('Enter your marks biology');
+// var Maths = +prompt('Enter your marks Maths');
+// var Urdu = +prompt('Enter your marks Urdu');
+// // var Phy = +prompt('Enter your marks Physics');
+  
+var totalsubject = chem + bio + Maths + Urdu + Phy ; 
+
+var totalmarks = 500 ;
+var totalpercent = totalsubject / totalmarks * 100
+console.log(totalpercent);
+
+var getInput = document.querySelector('.input');
+console.log(getInput.value + totalpercent) 
+
+function percentageCal(){
+    // console.log(totalsubject/totalmarks*100);
+    console.log(getInput.value);
+    console.log(totalpercent);
+    input.value = ""
+}
+
+
+
+
+// var getInput = document.querySelector(".head");
+// console.log(input.value)
+
+
+// function percentageCal(){
+//     console.log(input.value);
+//     input.value = ""
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
