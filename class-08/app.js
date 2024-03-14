43f8682f20022d52e8ee4ae6a4164825998e48dc
Muhammad Faizan <fiazansuri855@gmail.com>
2024-03-15 01:32:22 +0500

    JS class Array practice section

---
+++ class-08/app.js
@@ -0,0 +1,284 @@
+// console.log('Hello Array') ;
+
+        //  var 
+
+        // var declare bhi hota hai
+        // var redeclare bhi hota hai
+        // var reassign bhin hota hai
+
+// var userName = 'muhammad' ;     //  declare
+// var userName = 'faizan' ;      // redeclare
+// userName = 'asim';              //      reassign
+
+// console.log(userName);
+
+
+
+
+     //   let 
+
+    // let redeclare nhi hota hain !
+    // let reassign hota hain ! 
+
+// let userName = 'muhammad' ;
+// let userName = 'faizan' ;
+// userName = 'asim' ;
+
+// console.log(userName);
+
+
+
+
+
+//        const 
+     
+//       const na redeclare hota hai or na he reassign hota ha
+
+// const userName = 'muhammad' ;
+// userName = 'faizan' ;
+
+// console.log(userName);
+
+
+
+
+
+// const fruits = ['apple' , 'banana' , 'orange' , 'mango' , 'strawberry'];
+
+// const fruits = ['apple' , 'banana' , 'orange' , 'mango' , 'strawberry' , ['pineapple' , 'melon' , 'grapes']];
+
+
+// console.log(fruits);
+// console.log(fruits.length);
+// console.log(typeof(fruits));
+// console.log(fruits[3]);
+// console.log(fruits[5][2]);
+
+// console.log(`${fruits[0]} ${fruits[5][0]}`);
+
+
+
+
+
+
+
+
+
+
+
+
+    // array method 
+
+
+//push
+//pop
+//unshift
+//shift
+//splice
+//slice
+//split
+//join
+//concat
+//sort
+//reverse
+//indexof
+
+
+
+       // push akhr mai add karta ha value ko
+
+// const fruits = ['apple' , 'banana' , 'orange' , 'mango' , 'strawberry'];
+// fruits.push('grapes');
+
+// console.log(fruits)
+
+
+
+     // pop      akhr se value hata deta ha
+
+// const fruits = ['apple' , 'banana' , 'orange' , 'mango' , 'strawberry'];
+// fruits.pop()
+
+// console.log(fruits)
+ 
+
+
+     // unshift      shuru mai value add karta ha
+
+
+// const fruits = ['apple' , 'banana' , 'orange' , 'mango' , 'strawberry'];
+// fruits.unshift('grapes')
+
+// console.log(fruits);
+
+     
+//        shift        shuru se value ko hata deta hain
+
+
+// const fruits = ['apple' , 'banana' , 'orange' , 'mango' , 'strawberry'];
+// fruits.shift();
+
+// console.log(fruits)
+
+
+
+
+//      concat       2 array ko ek sath concat kar deta hai
+
+
+
+// const num = ['Muhammad' , 'Faizan' , 'Asim'];
+// const num1 = ['Talha' , 'sameer' , 'ali'];
+
+// const myArr = num.concat(num1);
+// console.log(myArr)
+
+
+
+
+
+  //     sort       start se alphabet se use hota ha
+
+
+
+
+// const fruits = ['orange' , 'mango' , 'apple' , 'banana'];
+// fruits.sort();
+
+// console.log(fruits);
+
+
+// const point = [40 , 60 , 100 , 5 , 1 , 10 ,30];
+// point.sort(function(b , c){return b - c});
+// console.log(point);
+
+
+
+
+
+
+//     Reverse          array method Reverse hote ha 
+
+
+
+
+// const fruits = ['orange' , 'mango' , 'apple' , 'banana'];
+// fruits.reverse()
+
+// console.log(fruits);
+
+
+  
+//     indexof 
+
+
+
+// const fruits = ['banana' , 'orange' , 'grapes' , 'apple' , 'strawberry']
+// const index = fruits.indexOf('apple' , 3)
+
+// console.log(index);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
