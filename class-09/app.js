35ad420a441aa23dc846b1d79165deed4e127f63
Muhammad Faizan <fiazansuri855@gmail.com>
2024-03-14 23:17:14 +0500

    JS loops class

---
+++ class-09/app.js
@@ -0,0 +1,150 @@
+// console.log('hello loops');
+
+
+
+
+// forloops
+// while loops
+// do while loops 
+
+
+// for(let i = 1 ; i <= 10; i++){
+//     console.log('hello world', i);
+// }
+
+
+
+
+
+// const userInput = +prompt('enter your table');
+
+// for(let i = 1 ; i <= 10 ; i++){
+//     console.log(`${userInput} * ${i} = ${userInput*i}`);
+// }
+
+
+
+
+
+
+
+// const userInput = +prompt('enter your table');
+// const times = +prompt('how many times you want');
+
+
+// for(let i = 1 ; i <= times ; i++){
+//     console.log(`${userInput} * ${i} = ${userInput*i}`)
+// }
+
+
+
+
+
+
+// for (let i = 0 ; i < 10 ; i++){
+//     console.log('outer loop' , i)
+//     for (let j = 1 ; j < 10 ; j++){
+//         console.log('inner loop' , i)
+//     }
+// }
+
+
+
+
+
+
+
+// for (let i = 1 ; i <= 100 ; i++){
+//     console.log(i)
+//     for (let j = 1 ; j <= 10; j++){
+//         console.log(`${i} * ${j} = ${i*j}`)
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
