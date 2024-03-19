20c4ba4bff6e5b4ab0bae6b3beeffe163b8ad6e9
Muhammad Faizan <fiazansuri855@gmail.com>
2024-03-19 06:42:47 +0500

    JS loops Practice section

---
+++ JS-Practice/Loops-Practice/app.js
@@ -0,0 +1,80 @@
+// console.log('hello loops')
+
+
+// for loops
+
+
+// for(let i = 1 ; i <= 10 ; i++){
+//     console.log(i);
+// }
+
+
+
+
+
+
+// for(let i = 1 ; i <= 10 ; i++){
+//     console.log(`${9} * ${i} = ${9 * i}`)
+// }
+
+
+
+
+
+
+// for(let i = 1 ; i <= 100 ; i++){
+//     console.log(i)
+//     for(let j = 1 ; j <= 10 ; j++){
+//         console.log(`${i} * ${j} = ${i *j}`)
+//     }
+// }
+
+
+
+
+
+
+
+// const fruits = ['apple' , 'mango' , "orange" , 'banana' , 'strawberry' ];
+// console.log(fruits)
+
+// for(let i = 0 ; i < fruits.length ; i++){
+//     console.log(fruits[i]);
+// }
+
+
+
+
+
+
+
+
+
+
+// const fruits = ['apple' , 'mango' , "orange" , 'banana' , 'strawberry' ];
+// console.log(fruits)
+
+// const ul = document.querySelector('ul');
+
+// for(let i = 0 ; i < fruits.length ; i++){
+//     console.log(fruits[i]);
+
+//     ul.innerHTML += `<li>${fruits[i]}</li>` 
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
