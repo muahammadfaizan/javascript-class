4ab81c4adb526f8aaf0372f027e1c83e19942235
Muhammad Faizan <fiazansuri855@gmail.com>
2024-05-24 23:27:20 +0500

    Array Loops

---
+++ Array Loops/app.js
@@ -0,0 +1,66 @@
+// console.log('hello world');
+
+
+
+// Array Loops
+
+
+// map
+// foreach
+// filter
+// reduce
+
+
+const arr = ['Apple' , 'Banana' , 'orange' , 'mango' , 'gavua'];
+
+const div = document.querySelector('div')
+
+// arr.map((item)=>{
+//     console.log(item);
+// })
+
+
+
+// arr.forEach((item , index) => {
+//     // console.log(item)
+//     div.innerHTML += `<h1>${index}. ${item}</h1>`
+// })
+
+
+
+
+
+arr.filter((item) => {
+    console.log(item)
+} )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
